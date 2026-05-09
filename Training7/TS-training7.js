47c2624fbbcd57c3182796a59f6c616da056f923
NotVenuly <147390259+NotVenuly@users.noreply.github.com>
2026-05-09 09:43:39 +0300

    folder for all training and completed training 7
    Learned: Advanced DOM: Keyboard Events & Modal Logic

---
+++ Training7/TS-training7.js
@@ -0,0 +1,48 @@
+"use strict";
+const modalBtn = document.getElementById("openModalBtn");
+const overlay = document.getElementById("modalOverlay");
+const incr = document.getElementById("incrementBtn");
+const decr = document.getElementById("decrementBtn");
+const reset = document.getElementById("resetBtn");
+const box = document.getElementById("modalBox");
+const closeBtn = document.getElementById("modalCloseBtn");
+let outsideCounter = document.getElementById("outsideCounter");
+let insideCounter = document.getElementById("modalCounter");
+let currNumber = 0;
+incr.addEventListener("click", () => {
+    currNumber += 1;
+    insideCounter.textContent = `${currNumber}`;
+});
+decr.addEventListener("click", () => {
+    currNumber -= 1;
+    insideCounter.textContent = `${currNumber}`;
+});
+reset.addEventListener("click", () => {
+    currNumber = currNumber - currNumber;
+    insideCounter.textContent = `${currNumber}`;
+});
+modalBtn.addEventListener("click", () => {
+    OpenModal();
+});
+closeBtn.addEventListener("click", () => {
+    CloseModal();
+});
+document.addEventListener("keydown", (event) => {
+    if (event.key.toLowerCase() === "escape" || event.key.toLowerCase() === "x") {
+        CloseModal();
+    }
+});
+overlay.addEventListener("click", (event) => {
+    if (event.target === overlay) {
+        CloseModal();
+    }
+});
+function OpenModal() {
+    overlay.classList.add("visible");
+    overlay.setAttribute("aria-hidden", "false");
+}
+function CloseModal() {
+    overlay.classList.remove("visible");
+    overlay.setAttribute("aria-hidden", "true");
+    outsideCounter.textContent = insideCounter.textContent;
+}
